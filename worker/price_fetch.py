bfd4e85636e52ff54296f531acb237c845c98b56
Jenil2503 <jenilrupareliya2503@gmail.com>
2026-09-24 21:28:11 +0530

    Price fetch changed to 5d and 5min bars

--- worker/price_fetch.py
+++ worker/price_fetch.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import pandas as pd
 
-def fetch_ohlc(ticker : str, period = '25d', interval : str = '15m'):
+def fetch_ohlc(ticker : str, period = '15d', interval : str = '5m'):
     df = yf.download(ticker, period = period, interval = interval, progress = False)
     
     if df.empty:
