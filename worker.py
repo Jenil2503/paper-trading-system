95a668c9cb64c60f649475dd9b3ebe979aa77f15
Jenil2503 <jenilrupareliya2503@gmail.com>
2026-09-25 01:23:55 +0530

    worker.py fixed, iteratin for all accounts

--- worker.py
+++ worker.py
@@ -7,7 +7,7 @@ def get_test_account():
     if not result.data:
         raise ValueError("No accounts found - signup as user first")
     
-    return result.data[0]
+    return result
 
 def get_position(user_id : int, symbol : str):
     result = supabase.table("positions") \
@@ -32,9 +32,11 @@ def execute_buy(user_id : int, symbol : str, price : float, quantity : int):
         print(f"Not enough to buy {quantity} {symbol} at {price}")
         return
     
+
+    
     supabase.table("trades").insert({
         "user_id" : user_id, "symbol" : symbol, "side" : "BUY",
-        "quantity" : quantity, "price" : price
+        "quantity" : int(float(quantity)), "price" : price
     }).execute()
     
     existing = get_position(user_id, symbol)
@@ -44,7 +46,7 @@ def execute_buy(user_id : int, symbol : str, price : float, quantity : int):
     else:
         supabase.table("positions").insert({
             "user_id" : user_id, "symbol" : symbol,
-            "quantity" : quantity, "avg_entry_price" : price
+            "quantity" : int(float(quantity)), "avg_entry_price" : price
         }).execute()
     
     supabase.table("accounts").update({
@@ -67,7 +69,7 @@ def execute_sell(user_id : int, symbol : str, price : float):
     supabase.table("trades").insert({
         "user_id" : user_id, 
         "symbol" : symbol, "side" : "SELL",
-        "quantity" : quantity, "price" : price
+        "quantity" : int(float(quantity)), "price" : price
     }).execute()
     
     supabase.table("positions").update({"quantity" : 0}).eq("id",position["id"]).execute()
@@ -81,35 +83,41 @@ def execute_sell(user_id : int, symbol : str, price : float):
     
         
 def run(): 
-    account = get_test_account()
-    user_id = account['user_id']
-    symbol = account["tracked_symbol"]
-    
-    df = fetch_ohlc(symbol)
-    df = generate_signals(df)
-    latest = df.iloc[-1]
-    
-    price = float(latest["Close"])
-    atr = float(latest["atr"])
-    entry = int(latest["entry_signal"])
-    exit_ = int(latest["exit_signal"])
-    
-    print(f"[user {user_id}] {symbol}@ {price} | entry = {entry} exit = {exit_}")
-    
-    existing_position = get_position(user_id, symbol)
-    
-    if entry == 1 and not existing_position:
-        dollar_risk = 0.01*account["cash_balance"]
-        quantity = max(1,int(dollar_risk/atr))
-        execute_buy(user_id,symbol, price, quantity)
-    elif entry == 1 and existing_position:
-        print("Entry signal fired, but already holding a postion - skipping.")
-    elif exit_ == 1 and existing_position:
-        execute_sell(user_id, symbol, price)
-    elif entry == 1 and not existing_position:
-        print("Exit signal fired, but no holding a postion - skipping.")
-    else:
-        print("No Signal - Holding")
+    accounts = get_test_account()
+    accounts = accounts.data
+    # accounts = accounts[1
+    for account in accounts:
+        
+        user_id = account["user_id"]
+        symbol = account["tracked_symbol"]
+        
+        df = fetch_ohlc(symbol)
+        df = generate_signals(df)
+        latest = df.iloc[-1]
+        
+        price = float(latest["Close"])
+        unit_size = float(latest["unit_size"])
+        # unit_size = unit_size//10
+        entry = int(latest["entry_signal"])
+        exit_ = int(latest["exit_signal"])
+        
+        print(f"[user {user_id}] {symbol}@ {price} | entry = {entry} exit = {exit_}")
+        
+        existing_position = get_position(user_id, symbol)
+        
+        if entry == 1 and not existing_position:
+            # dollar_risk = 0.01*account["cash_balance"]
+            quantity = unit_size*account["cash_balance"]*.1
+           
+            execute_buy(user_id,symbol, price, quantity)
+        elif entry == 1 and existing_position:
+            print("Entry signal fired, but already holding a postion - skipping.")
+        elif exit_ == 1 and existing_position:
+            execute_sell(user_id, symbol, price)
+        elif entry == 1 and not existing_position:
+            print("Exit signal fired, but no holding a postion - skipping.")
+        else:
+            print("No Signal - Holding")
         
 if __name__ == "__main__":
     run()
